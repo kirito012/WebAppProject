f189db6403277355fe11e233ad804b981a2d637c
ItsOlivex <mirko.olivetti.f@gmail.com>
2022-07-13 09:35:14 +0200

    s

--- ProgettoWebApp/Client-Side/script/home/angular/main.js
+++ ProgettoWebApp/Client-Side/script/home/angular/main.js
@@ -24,31 +24,33 @@ let clicked = false;
 const app = angular.module('myApp', []);
 
 app.controller('myController', function($scope, $http, $timeout) {
-    getModels($scope, $http);
-    getMachines($scope, $http);
-    getProfile($scope, $http);
+    if(restart){
+        getModels($scope, $http);
+        getMachines($scope, $http);
+        getProfile($scope, $http);
 
-    button.addEventListener("click", changeProfileData($scope, $http));
+        button.addEventListener("click", changeProfileData($scope, $http));
 
-    formProfile.addEventListener("submit", () => {
-        getNewPfp($scope, $http);
-        formProfile.querySelector(".fileInput").value = "";
-    });
+        formProfile.addEventListener("submit", () => {
+            getNewPfp($scope, $http);
+            formProfile.querySelector(".fileInput").value = "";
+        });
 
-    formMachines.addEventListener("submit", getMachines($scope, $http));
+        formMachines.addEventListener("submit", getMachines($scope, $http));
 
-    submitAddMachine.addEventListener("click", addMachine($scope, $http));
+        submitAddMachine.addEventListener("click", addMachine($scope, $http));
 
-    $scope.myFunction = function(index){
-        clicked = false;
-        inputValue.value = index;
-    }
-    $scope.disable = function(){
-        clicked = true;
-    }
-    $scope.mouseOut = function(){
-        if(!clicked){
-            inputValue.value = '';
+        $scope.myFunction = function(index){
+            clicked = false;
+            inputValue.value = index;
+        }
+        $scope.disable = function(){
+            clicked = true;
+        }
+        $scope.mouseOut = function(){
+            if(!clicked){
+                inputValue.value = '';
+            }
         }
     }
 
